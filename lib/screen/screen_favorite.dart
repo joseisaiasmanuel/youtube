37672f04f69bd38427557bc8e2f93d900da15be3
jose1704 <joseisaiasmanuel@gmail.com>
2021-03-16 01:37:09 +0100

    finalizada

--- lib/screen/screen_favorite.dart
+++ lib/screen/screen_favorite.dart
@@ -1,56 +1,54 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:youtube/api.dart';
 import 'package:youtube/blocs/favorites_blocs.dart';
 import 'package:youtube/models/video.dart';
 
 class ScreenFavorite extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final bloc=BlocProvider.of<FavoriteBloc>(context);
+    final bloc = BlocProvider.of<FavoriteBloc>(context);
     return Scaffold(
       appBar: AppBar(
-        title:Text("Favoriotos"),
+        title: Text("Favoriotos"),
         centerTitle: true,
         backgroundColor: Colors.black87,
       ),
       backgroundColor: Colors.black87,
-      body:StreamBuilder<Map<String, Video>>(
-        stream:bloc.autFive,
+      body: StreamBuilder<Map<String, Video>>(
+        stream: bloc.autFive,
         initialData: {},
-        builder: (context,snapshot){
+        builder: (context, snapshot) {
           return ListView(
-            children:snapshot.data.values.map((v){
+            children: snapshot.data.values.map((v) {
               return InkWell(
-                onTap: (){
-
+                onTap: () {
+                  FlutterYoutube.playYoutubeVideoById(
+                      apiKey: API_Key, videoId: v.id);
                 },
-                onLongPress:(){
+                onLongPress: () {
                   bloc.toggleFavorite(v);
-
-                } ,
+                },
                 child: Row(
-                  children:<Widget> [
+                  children: <Widget>[
                     Container(
                       width: 100,
                       height: 50,
                       child: Image.network(v.thunb),
                     ),
                     Expanded(
-                      child: Text(v.title, style:TextStyle(color:Colors.white70),
-                      maxLines: 2,
+                      child: Text(
+                        v.title,
+                        style: TextStyle(color: Colors.white70),
+                        maxLines: 2,
                       ),
-
                     )
                   ],
                 ),
-
               );
             }).toList(),
-
-
           );
-
-
         },
       ),
     );
